ae3f741f0fbfc6afc42c0105479306a3bd6e2cbc
RoachLok <jtabordajunior@hotmail.com>
2020-12-19 03:38:32 +0100

    Client-side rendering with js and api.   - app.py : Code cleanup and restructuring. Old POST method to display new matches on reload removed.    New method that serves as API local_corpus_match that returns rendered tabpane template containing matches of selected file or json containing top matches for POST text content.   - text_simi_matches.html : template from which the tabpane containing the new matches is created.   - text_simi.html : Some styling changes. Added JS script to request for new file matches. Replaces resulting tabpane with new one.    The texts to be checked are now dynamic (no page reloading required).

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, g, jsonify
 from texts import *
 
 app = Flask(__name__)
+
+# TODO: Style matches container. Progress bar in matches dropdown. 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-Checks  = Check_texts()
-Checks_Names = get_filenames_from_paths(Checks )
-
-@app.route('/text_simi', methods=['GET', 'POST'])
-def text_simi():
+#  ----   TEXT_SIMI API (text-matches html generator)   ----  #   
+@app.route('/local_corpus_match/<file_name>', methods=['GET', 'POST'])
+def local_corpus_match(file_name):
     if request.method == 'POST':
-        choice       = request.form['file_selector']
+        text = str(request.get_data())
+        return jsonify(getMatchesText(text, Samples_Corpus)), 201
 
-        file_text    = get_text_from_file_name(choice)
-        file_matches = getMatchesText(file_text, Samples_Corpus) 
+    return render_template('text_simi_matches.html', file_matches = getMatchesText(get_text_from_file_name(file_name), Samples_Corpus)), 201
 
-        return render_template('text_simi.html', sample_names = Samples_Corpus, check_names = Checks_Names,
-                                file_text = file_text, selected_file = choice, file_matches = file_matches)
-    
-    file_text   = get_text_from_file_name(Checks_Names[0])
-    #sample_text = get_text_from_file_name(Sample_Names[0], False)
-    return render_template('text_simi.html', sample_names = ["Nothing to show", ""], check_names = Checks_Names,
-                            file_text = file_text, selected_file = Checks_Names[0])
+#  ----   TEXT_SIMI MAIN VIEW   ----  #   
+Checks  = Check_texts()
+Checks_Names = get_filenames_from_paths(Checks)
+
+Checks_Texts = []
+for file_name in Checks_Names:
+    Checks_Texts.append(get_text_from_file_name(file_name))
+
+@app.route('/text_simi')
+def text_simi():
+    return render_template('text_simi.html', check_names = Checks_Names, file_texts = Checks_Texts,
+                            file_matches = getMatchesText(get_text_from_file_name(Checks_Names[0]), Samples_Corpus))
 
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
-
-
-
-"""
-Samples = Sample_texts()
-Samples_Corpus = sample_corpus_from_path(Samples)
-#Sample_Names = get_filenames_from_paths(Samples)   # Removable
-"""
